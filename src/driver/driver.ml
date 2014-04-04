7f2093a6625ba8d57d7730bd82e9e4169d72fdee
ramntry <ramntry@gmail.com>
2014-04-05 03:30:44 +0400

    simple driver was done. Usage: [sll/src/driver] ./driver <source file>. Run make for example

---
+++ src/driver/driver.ml
@@ -0,0 +1,28 @@
+let read_file fname =
+  let in_file = open_in fname in
+  let len = in_channel_length in_file in
+  let buf = String.create len in
+  really_input in_file buf 0 len;
+  close_in in_file;
+  buf
+
+let c_compiler_command =
+  "gcc -std=c99 -x c -O2 -I ../backend ../backend/runtime.c -o a.out -"
+
+let compile fname =
+  let gen program =
+    let c_program = Emit_c.emit program in
+    let c_compiler_channel = Unix.open_process_out c_compiler_command in
+    output_string c_compiler_channel c_program;
+    close_out c_compiler_channel
+  in
+  Parser.parse (read_file fname) gen
+
+let print_usage () =
+  Printf.printf "Usage: %s <source file>\n" Sys.argv.(0)
+
+let () =
+  if Array.length Sys.argv <> 2 then
+    print_usage ()
+  else
+    compile Sys.argv.(1)
---
+++ src/parser/parser_test.ml
@@ -0,0 +1,2 @@
+let () =
+  Parser.big_test ()
